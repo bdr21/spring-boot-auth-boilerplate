3baee00c3ecea7c921b6da802dab2e211a3df9ed
bdr21 <omarbdr1999@gmail.com>
2022-06-10 21:42:40 +0100

    init

--- src/main/java/io/peppermint100/server/constant/Controller.java
+++ src/main/java/io/peppermint100/server/constant/Controller.java
@@ -1,6 +1,6 @@
 package io.peppermint100.server.constant;
 
 public class Controller {
-    public static final String SIGN_UP_SUCCESS_MESSAGE = "회원가입에 성공하였습니다.";
-    public static final String LOG_IN_SUCCESS_MESSAGE = "로그인에 성공하였습니다." ;
+    public static final String SIGN_UP_SUCCESS_MESSAGE = "You have successfully registered as a member.";
+    public static final String LOG_IN_SUCCESS_MESSAGE = "Login was successful." ;
 }
